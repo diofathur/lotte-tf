e8194aa9a8bc61cc9378e5e451697ae6974ae931
diofathur <diofathur27@gmail.com>
2024-09-04 10:12:40 +0700

    fix key

--- key.tf
+++ key.tf
@@ -1,5 +1,5 @@
 resource "aws_key_pair" "pti-key" {
   key_name = "${local.name}-key"
-  public_key = file("~/id_rsa.pub")
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCIzE5PfmUYrR9LRxfO6rMa9mGxNGaVBfc4CEYBIOehNFjqiVlYYGDulrOKK1tKTzPDTfAgKhs9GDGKPiecy1jxAai2VlkXBXeJlGQRj3hkiUwfAp+zUBmOOquDyhGWdGe4Wug37BT/AysPYXG3OZnn2SWw6aYKaQ0pOAw31p7rq/JOJ/PmokI+oytOl7HIJA+dqpegFG1rWaBVRMU0Jgp42nNBBsZagDYGs9wSe5QUC1RzYmI9eCpq1LG4tv1fkFN/l2lw+x6RB+b1lgN1XTAmDu8/OFXln0Fju6NUVcXSV/lUZtnkQ6LTmNx/yP5iiVRPLR8B2KiO8oXRzuGMyTD/ rsa-key-20240904"
 }
 
