b6dab3ad1997e5b29bedc8ee1974e32cfc4748d7
dio fathur <diofathur27@gmail.com>
2025-04-08 15:52:42 +0700

    fix

--- s3.tf
+++ s3.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "name" {
+resource "aws_s3_bucket" "testing-s3" {
   bucket = "pti-veeam-backup-bucket" // don't forget to change this name 
   object_lock_configuration {
     object_lock_enabled = "Enabled"
