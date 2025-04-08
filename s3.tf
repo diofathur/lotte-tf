3936e8df90ff0081cee5c2acbe82bdc573a982d7
dio fathur <diofathur27@gmail.com>
2025-04-08 15:40:16 +0700

    new

--- s3.tf
+++ s3.tf
@@ -1,5 +1,19 @@
 resource "aws_s3_bucket" "name" {
   bucket = "pti-veeam-backup-bucket" // don't forget to change this name 
+  object_lock_configuration {
+    object_lock_enabled = "enabled"
+
+    rule {
+      default_retention {
+        mode = "GOVERNANCE"
+        days = 365
+      }
+    }
+  }
+
+  versioning {
+    enabled = true
+  }
 
   tags = {
     Name = "pti-veeam-backup-bucket" // and this
